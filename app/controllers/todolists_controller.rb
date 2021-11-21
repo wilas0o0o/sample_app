62508f0034f0700bd6ca4f2be4ec75272aa7df29
wilas0o0o <03mcu30@gmail.com>
2021-11-21 13:04:58 +0000

    [Finish]2-5

--- app/controllers/todolists_controller.rb
+++ app/controllers/todolists_controller.rb
@@ -4,9 +4,17 @@ class TodolistsController < ApplicationController
   end
   
   def create
-    @list=List.new(list_params)
-    @list.save
-    redirect_to "/top"
+    list=List.new(list_params)
+    list.save
+    redirect_to todolist_path(list.id)
+  end
+  
+  def index
+    @lists=List.all
+  end
+  
+  def show
+    @list=List.find(params[:id])
   end
   
   private
